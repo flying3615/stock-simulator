f5935017033f63578ee7ba6a9841f522664f3fe6
yufei <gabriel.liu3615@gmail.com>
2025-11-01 10:41:59 +1300

    feat: enhance playback functionality with automatic scrolling and historical data fetching

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -14,6 +14,7 @@ export interface ChartRef {
   resetCrop: () => void;
   startCrop: () => void;
   fitContent: () => void;
+  resize: (width: number, height: number) => void;
 }
 
 interface ChartProps {
